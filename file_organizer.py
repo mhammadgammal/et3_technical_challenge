745d8ecd9f765563a3770881ba81cb44442a154f
mhammadgammal <mhammadgamal17@gmail.com>
2025-08-12 02:33:23 +0300

    implemnt method to organize files based on provided folder path

--- file_organizer.py
+++ file_organizer.py
@@ -34,7 +34,7 @@ categories = {
     ]
 }
 
-def organize_files(file_path):
+def get_category(file_path):
     # extract file extension
     ext = os.path.splitext(file_path)[1].lower()
 
@@ -45,4 +45,30 @@ def organize_files(file_path):
 
     # If no category matched, return "Others"
     return "Others"
+
+def print_summary(summary):
+    print("\nSummary:")
+    for category, count in summary.items():
+        print(f"{category}: {count} files")
+
+
+def organize_files(folder_path, sim = False):
+    summary = defaultdict(int)
+
+    for item in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item)
+        if os.path.isfile(item_path):
+            category = get_category(item_path)
+            target_folder = os.path.join(folder_path, category)
+            if not os.path.exists(target_folder):
+                os.makedirs(target_folder)
+
+            summary[category] += 1
+
+            if sim:
+                print(f"Simulating move: {item_path} -> {target_folder}")
+            else:
+                shutil.move(item_path, target_folder)
+    
+    print_summary(summary)
 
